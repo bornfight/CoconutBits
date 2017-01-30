e0e811f97ad38cf96d7b95e06db0edbb9160d6f1
Filip Fajdetic <fajdof@gmail.com>
2017-01-30 14:23:59 +0100

    added delay

---
+++ EssentialPod/Classes/Delay.swift
@@ -0,0 +1,16 @@
+//
+//  Delay.swift
+//  Pods
+//
+//  Created by Filip Fajdetic on 30/01/2017.
+//
+//
+
+import Foundation
+
+public func delay(seconds: Double, queue: DispatchQueue , completion: ()->Void) {
+    let popTime = DispatchTime(uptimeNanoseconds: UInt64(seconds*Double(NSEC_PER_SEC)))
+    queue.asyncAfter(deadline: popTime) { 
+        completion()
+    }
+}
