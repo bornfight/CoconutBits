00360d0aa5d2c8286828603bf1c903929e259cf8
Filip Fajdetic <fajdof@gmail.com>
2017-02-14 15:18:59 +0100

    added ifSome

---
+++ EssentialPod/Classes/IfSome.swift
@@ -0,0 +1,27 @@
+//
+//  IfSome.swift
+//  Pods
+//
+//  Created by Filip Fajdetic on 14/02/2017.
+//
+//
+
+import Foundation
+
+
+public extension Optional {
+    // `ifSome` function executes the closure if there is some value
+    func ifSome(_ handler: (Wrapped) -> Void) {
+        switch self {
+        case .some(let wrapped): return handler(wrapped)
+        case .none: break
+        }
+    }
+    
+    func ifNil(_ handler: (Wrapped) -> Void) {
+        switch self {
+        case .none(let wrapped): return handler(wrapped)
+        case .some: break
+        }
+    }
+}
