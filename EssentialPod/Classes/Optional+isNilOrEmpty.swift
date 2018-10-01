c2a4bc79517d9147a1223a3ae84431180e66a347
Dino <dino.srdoc@degordian.com>
2018-10-01 13:27:19 +0200

    add fancy isNilOrEmpty method

---
+++ EssentialPod/Classes/Optional+isNilOrEmpty.swift
@@ -0,0 +1,14 @@
+//
+//  Optional+isNilOrEmpty.swift
+//  EssentialPod
+//
+//  Created by Dino on 01/10/2018.
+//
+
+import Foundation
+
+public extension Optional where Wrapped: Collection {
+    var isNilOrEmpty: Bool {
+        return map { $0.isEmpty } ?? true
+    }
+}
