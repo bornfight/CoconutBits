e2cfb23c788fb66c317319ee7a20c17ab28a6da3
Filip Fajdetic <fajdof@gmail.com>
2018-10-03 10:32:55 +0200

    update to swift 4.2

--- CoconutBits/Classes/Alert.swift
+++ CoconutBits/Classes/Alert.swift
@@ -11,9 +11,9 @@ import UIKit
 
 
 public extension UIViewController {
-    func showAlertMessage(_ message: String, title: String, actionTitle: String, style: UIAlertControllerStyle = UIAlertControllerStyle.alert) {
+    func showAlertMessage(_ message: String, title: String, actionTitle: String, style: UIAlertController.Style = UIAlertController.Style.alert) {
         let nameEmptyAlertController = UIAlertController(title: title, message: message, preferredStyle: style)
-        let defaultAction = UIAlertAction(title: actionTitle, style: UIAlertActionStyle.default, handler: nil)
+        let defaultAction = UIAlertAction(title: actionTitle, style: UIAlertAction.Style.default, handler: nil)
         nameEmptyAlertController.addAction(defaultAction)
         self.present(nameEmptyAlertController, animated: true, completion: nil)
     }
