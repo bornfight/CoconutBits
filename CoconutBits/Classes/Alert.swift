bdd992007edcaf1cf64c68938835cadab92521ab
Dino <dino.srdoc@degordian.com>
2018-10-17 15:29:56 +0200

    name alert controller "alert controller"

--- CoconutBits/Classes/Alert.swift
+++ CoconutBits/Classes/Alert.swift
@@ -12,9 +12,9 @@ import UIKit
 
 public extension UIViewController {
     func showAlertMessage(_ message: String, title: String, actionTitle: String, style: UIAlertController.Style = UIAlertController.Style.alert) {
-        let nameEmptyAlertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
         let defaultAction = UIAlertAction(title: actionTitle, style: UIAlertAction.Style.default, handler: nil)
-        nameEmptyAlertController.addAction(defaultAction)
-        self.present(nameEmptyAlertController, animated: true, completion: nil)
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
     }
 }
