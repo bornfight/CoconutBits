e4efe93e2536ed90fd17f3c1816245b9baef5c5e
Dino Srdoč <dino.srdoc@bornfight.com>
2019-11-20 09:08:00 +0100

    add nib instantiable

---
+++ CoconutBits/Classes/NibInstantiable.swift
@@ -0,0 +1,22 @@
+//
+//  NibInstantiable.swift
+//  CoconutBits
+//
+//  Created by Dino on 20/11/2019.
+//
+
+import Foundation
+
+public protocol NibInstantiable { }
+
+public extension NibInstantiable where Self: UIViewController {
+    static func instantied() -> Self {
+        .init(nibName: String(describing: self), bundle: nil)
+    }
+}
+
+public extension NibInstantiable where Self: UIView {
+    static func instantied() -> Self {
+        .init(nibName: String(describing: self), bundle: nil)
+    }
+}
